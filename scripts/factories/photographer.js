19397ec51ffa74f989edee576e9d80fe3f64a56f
Zineb Katim <zkatim1301@gmail.com>
2023-06-08 19:24:11 +0200

    home page

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,17 +1,32 @@
 function photographerFactory(data) {
-    const { name, portrait } = data;
-
-    const picture = `assets/photographers/${portrait}`;
+    const { name, portrait, city, country, tagline, price, id} = data;
+    const picture = `./assets/photographers/${portrait}`;
+    // const idphotographe = `${id}`;
+    // console.log(idphotographe);
 
     function getUserCardDOM() {
         const article = document.createElement( 'article' );
+        //article.classList.add("card-photographer");
+
         const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
-        const h2 = document.createElement( 'h2' );
+        img.setAttribute("src", picture);
+        img.setAttribute("alt", `portrait du photographe ${name}`);
+        const h2 = document.createElement('h2');
         h2.textContent = name;
+        const h3 = document.createElement('h3');
+        h3.textContent = city + ", " +country;
+        const p = document.createElement("p");
+        p.textContent = tagline;
+        const div = document.createElement("div");
+        div.textContent = price +"€/jour";
+        div.setAttribute("role", "prix par jour")
+
         article.appendChild(img);
         article.appendChild(h2);
+        article.appendChild(h3);
+        article.appendChild(p);
+        article.appendChild(div);
         return (article);
     }
     return { name, picture, getUserCardDOM }
-}
+};
--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,46 +1,31 @@
-    async function getPhotographers() {
-        // Ceci est un exemple de données pour avoir un affichage de photographes de test dès le démarrage du projet, 
-        // mais il sera à remplacer avec une requête sur le fichier JSON en utilisant "fetch".
-        let photographers = [
-            {
-                "name": "Ellie-Rose Wilkens",
-                "id": 930,
-                "city": "Paris",
-                "country": "France",
-                "tagline": "Capturer des compositions complexes",
-                "price": 250,
-                "portrait": "EllieRoseWilkens.jpg"
-            },
-            {
-                "name": "Mimi Keel",
-                "id": 243,
-                "city": "London",
-                "country": "UK",
-                "tagline": "Voir le beau dans le quotidien",
-                "price": 400,
-                "portrait": "MimiKeel.jpg"
-            },
-        ]
-        // et bien retourner le tableau photographers seulement une fois récupéré
-        return ({
-            photographers: [...photographers, ...photographers, ...photographers]})
-    }
 
-    async function displayData(photographers) {
-        const photographersSection = document.querySelector(".photographer_section");
+async function getPhotographers() {
+// récuperation du tableau datas
+	let response = await fetch("./data/photographers.json");
+	//console.log(response);
+	const datas = await response.json();
+	//console.log(datas);
+	return datas;
+}
+;
+async function displayData(photographers) {
+	const photographersSection = document.querySelector(".photographer_section");
 
-        photographers.forEach((photographer) => {
-            const photographerModel = photographerFactory(photographer);
-            const userCardDOM = photographerModel.getUserCardDOM();
-            photographersSection.appendChild(userCardDOM);
-        });
-    };
+	photographers.forEach((photographer) => {
+		const photographerModel = photographerFactory(photographer);
+		const userCardDOM = photographerModel.getUserCardDOM();
+		photographersSection.appendChild(userCardDOM);
+	});
+	console.log(photographers)
+};
 
-    async function init() {
-        // Récupère les datas des photographes
-        const { photographers } = await getPhotographers();
-        displayData(photographers);
-    };
-    
-    init();
-    
+
+
+async function init() {
+	// Récupère les datas des photographes
+	const { photographers } = await getPhotographers();
+	displayData(photographers);
+};
+
+init();
+
